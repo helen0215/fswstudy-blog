eac6b7e76215c8f00484eee6ebf0e11fb7577605
helen0215 <seocc0604@gmail.com>
2022-07-13 21:27:47 +0900

    body-parser 추가

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express, {Request, Response} from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req: Request, res: Response) => {
   const name = process.env.NAME || 'World';
