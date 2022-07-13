81abfe1af821392239da8321c169718472d1a866
helen0215 <seocc0604@gmail.com>
2022-07-13 21:49:42 +0900

    Morgan (logging platform) 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('combined'));
 
 app.get('/', (req: Request, res: Response) => {
   const name = process.env.NAME || 'World';
